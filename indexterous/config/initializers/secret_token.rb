7422a02c8fcb267dfb37c21b8d09a25bcd7aac6d
Alexander Ose <alex@alexose.com>
2012-08-03 11:04:20 -0700

    init

---
+++ indexterous/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indexterous::Application.config.secret_token = 'eba567baa3af8eabafd47c5a02177f91b66f6bfb502537427c5d725219d6fb0251effd362f3ca1497d92edc62d5cd402f60088dea381acf957f1fcee63b1e00a'
